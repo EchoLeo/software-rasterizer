98ee80380a4eb2e11de24bed2ee0dd5e6acb2b2c
Nag1995 <18200135063@163.com>
2017-11-18 20:06:39 +0800

    the final of draw line

--- example.cc
+++ example.cc
@@ -1,4 +1,5 @@
 #include "pngimage.h"
+#include <algorithm>
 
 const int size = 256;
 const Color white = Color(255, 255, 255, 255);
@@ -92,13 +93,131 @@ void test_img_rgba()
     img.write_png_file("img_rgba.png");
 }
 
-void line(int x0, int y0, int x1, int y1, PNGImage &img, const Color &c)
+void line1(int x0, int y0, int x1, int y1, PNGImage &img, const Color &c)
+{
+    for (float t = 0; t < 1; t += 0.01)
+    {
+        int x = x0*(1-t) + x1*t;
+        int y = y0*(1.-t) + y1*t;
+        img.set(x, y, c);
+    }
+}
+
+void line2(int x0, int y0, int x1, int y1, PNGImage &img, const Color &c)
 {
     for (int x = x0; x <= x1; x++)
     {
         float t = (x - x0)/(float)(x1 - x0);
         int y = y0*(1-t) + y1*t;
         img.set(x, y, c);
+    }
+}
+
+void line3(int x0, int y0, int x1, int y1, PNGImage &img, const Color &c)
+{
+    bool steep = false;
+
+    if ( std::abs(x0-x1) < std::abs(y0-y1) )
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    for (int x = x0; x <= x1; x++)
+    {
+        float t = (x-x0) / (float)(x1-x0);
+        int y = y0*(1-t) + y1*t;
+        if (steep)
+        {
+            img.set(y, x, c);
+        }
+        else
+        {
+            img.set(x, y, c);
+        }
+    }
+}
+
+void line4(int x0, int y0, int x1, int y1, PNGImage &img, const Color &c)
+{
+    bool steep = false;
+    if (std::abs(x0-x1) < std::abs(y0-y1))
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    float derror = std::abs( (y1-y0)/(float)(x1-x0) );
+    float error = 0.f;
+    int y = y0;
+    for (int x = x0; x <= x1; x++)
+    {
+        if (steep)
+        {
+            img.set(y, x, c);
+        }
+        else
+        {
+            img.set(x, y, c);
+        }
+        
+        error += derror;
+        if (error >= 0.5f)
+        {
+            y += (y1>y0?1:-1);
+            error -= 1;
+        }
+    }
+}
+
+void line5(int x0, int y0, int x1, int y1, PNGImage &img, const Color &c)
+{
+    bool steep = false;
+    if (std::abs(x0-x1) < std::abs(y0-y1))
+    {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    int dx = std::abs(x1 - x0);
+    int derror = std::abs( (y1-y0) ) * 2;
+    int error = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++)
+    {
+        if (steep)
+        {
+            img.set(y, x, c);
+        }
+        else
+        {
+            img.set(x, y, c);
+        }
+        
+        error += derror;
+        if (error >= dx)
+        {
+            y += (y1>y0?1:-1);
+            error -= 2*dx;
+        }
     }
 }
 
@@ -109,9 +228,12 @@ int main()
     // test_img_rgb();
     // test_img_rgba();
     PNGImage img(size, size, PNGImage::Format::RGB);
-    line(13, 20, 80, 40, img, white);
-    line(20, 13, 40, 80, img, red); 
-    line(80, 40, 13, 20, img, red);
+    line5(13, 20, 80, 40, img, white);
+    line5(20, 13, 40, 80, img, red); 
+    line5(80, 40, 13, 20, img, red);
+    line5(30, 40, 30, 80, img, red);
+    line5(30, 40, 80, 40, img, red);
+
     img.write_png_file("line.png");
     return 0;
 }
